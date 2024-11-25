70a4d115f5e9dae0e457ab1aef01922abae16d12
jelemiekun <jeremieeden1946@gmail.com>
2024-11-25 19:17:43 +0800

    insertion sort

--- DSA/main.cpp
+++ DSA/main.cpp
@@ -90,6 +90,12 @@ static void insertionSort(int arr[], const int& size) {
 	for (int i = 1; i < size; ++i) {
 		int key = arr[i];
 		int j = i - 1;
+
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			--j;
+		}
+		arr[j + 1] = key;
 	}
 
 	printArray(arr, size);
